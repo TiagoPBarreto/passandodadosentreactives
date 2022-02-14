47300de9f429854cfc9c41cd9b11172cd670d4d4
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2022-02-14 16:03:05 -0300

    passandodadosentreactives

---
+++ app/src/main/java/com/cursoandroid/passandodadosentreactives/SegundaActivity.java
@@ -0,0 +1,30 @@
+package com.cursoandroid.passandodadosentreactives;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView textNome, textIdade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        textNome = findViewById(R.id.textNome);
+        textIdade = findViewById(R.id.textIdade);
+
+        //Reculperar os dado enviados
+        Bundle dados = getIntent().getExtras();
+        String nome = dados.getString("nome");
+        int idade = dados.getInt("idade");
+
+        // Configurar os valores reculperados
+
+        textNome.setText(nome);
+        textIdade.setText(String.valueOf(idade));
+    }
+}
